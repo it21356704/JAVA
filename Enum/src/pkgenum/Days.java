3cf82ccbcfbe7fdfd65b1e95c332f7cc7b7de467
IT21356704 <it21356704@my.sliit.lk>
2024-02-23 21:55:13 +0530

    Enum

---
+++ Enum/src/pkgenum/Days.java
@@ -0,0 +1,12 @@
+package pkgenum;
+
+
+public enum Days {
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY
+}
--- Enum/src/pkgenum/Enum.java
+++ Enum/src/pkgenum/Enum.java
@@ -1,20 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package pkgenum;
 
-/**
- *
- * @author chamigunawardane
- */
+
 public class Enum {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        for(Days values:Days.values()){
+//            System.out.println(values);
+        }
+        
+        Days var1 = Days.MONDAY;
+        String s1 = Days.FRIDAY.toString();
+        System.out.println(s1);
+        
     }
     
 }

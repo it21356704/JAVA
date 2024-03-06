f1150d53b372407130ecfaac209f5c4f57a5cab1
IT21356704 <it21356704@my.sliit.lk>
2024-03-06 22:38:22 +0530

    Inheritance

--- JavaInheritance/src/javainheritance/JavaInheritance.java
+++ JavaInheritance/src/javainheritance/JavaInheritance.java
@@ -21,12 +21,31 @@ class Mobile2020{
 }
 
 class Mobile2021 extends Mobile2020{
+    //25000 code lines
     
     @Override
     public void Call(String no){
-        super.Call(no);
+        super.Call(no);//5000
         super.NETWORK(no);
+        
+        //modification+++
+        System.out.println("Hi I'm a child.");
+        //20000
     }
+    
+    public void AI(){
+        System.out.println("AI");
+    }
+}
+
+final class Mobile2022 extends Mobile2021{
+    
+}
+
+class Mobile2021_V2 extends Mobile2020 {
+}
+
+class abc{
 }
 
 public class JavaInheritance {
@@ -37,6 +56,9 @@ public class JavaInheritance {
         
         Mobile2021 obj2 = new Mobile2021();
         obj2.Call("0858837484");
+        
+        Mobile2022 obj3 = new Mobile2022();
+        
     }
     
 }
--- VariableTypes/src/variabletypes/VariableTypes.java
+++ VariableTypes/src/variabletypes/VariableTypes.java
@@ -35,6 +35,35 @@ public class VariableTypes {
         //System.out.println(USD);
         System.out.println(version);
         
+        //String
+
+        String text = "Welcome to JAVA 2024";
+        
+        String FirstName=" Chami Gunawardane   ";
+        String SecondName=" ";
+
+        //String Methods
+        //(1) UPPER case
+        
+        String ans = text.toUpperCase();
+        String ans1 = FirstName.trim();
+        boolean ans2 = text.startsWith("W");
+        int ans3 = text.indexOf("2024");
+        String ans4 = text.replace("A","$");
+        
+        if(SecondName.isBlank()){
+            System.err.println("Second name is required");
+        }else{
+            System.out.println("Second name OK");
+        }
+        
+        System.out.println(text);
+        System.out.println(ans);
+        System.out.println(ans1);
+        System.out.println(ans2);
+        System.out.println(ans3);
+        System.out.println(ans4);
+        
     }
     
 }

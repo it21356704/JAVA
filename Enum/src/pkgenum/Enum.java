67220ee74c00f0f7ea397395e49675b2f804722f
IT21356704 <it21356704@my.sliit.lk>
2024-03-17 00:38:22 +0530

    Enum

--- Enum/src/pkgenum/Enum.java
+++ Enum/src/pkgenum/Enum.java
@@ -6,9 +6,9 @@ public class Enum {
 
     public static void main(String[] args) {
         
-        for(Days values:Days.values()){
+//        for(Days values:Days.values()){
 //            System.out.println(values);
-        }
+//        }
         
         Days var1 = Days.MONDAY;
         String s1 = Days.FRIDAY.toString();
